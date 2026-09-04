11ee16533a780a443e2a7e371bcc1031bf042aa5
ashcherbyna <anastasiavictorivna1@gmail.com>
2026-09-04 19:10:01 +0200

    Tasks 1-2

---
+++ HW4/task1.js
@@ -0,0 +1,15 @@
+
+const string = "Test string"
+console.log(string);
+
+const bool = true;
+console.log(bool);
+
+const num = 42;
+console.log(num);
+
+const nullValue = null;
+console.log(nullValue);
+
+const undefinedValue = undefined;
+console.log(undefinedValue);
---
+++ HW4/task2.js
@@ -0,0 +1,9 @@
+const ActorName = "Robert Downey Jr.";
+const ActorName2 = "Tom Holland";
+const ActorName3 = "Zendaya";
+
+const greeting1 = `Hello, ${ActorName}, ${ActorName2}, and ${ActorName3}! Welcome to the movie.`;
+console.log(greeting1);
+
+const greeting2 = "Hello, " + ActorName + ", " + ActorName2 + ", and " + ActorName3 + "! Welcome to the movie.";
+console.log(greeting2);
